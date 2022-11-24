dd50e66993f12722565a5b28cfe2b11f75e8f797
CampusNET-Primero <primero@campusnet.es>
2022-11-24 15:52:13 +0100

    Agregar archivos de proyecto.

---
+++ Pacman-22-Parte1/Pacman-22-Parte1.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#define MAP_VERTICAL 29
+#define MAP_HORIZONTAL 120
+enum TILES {EMPTY = ' ', WALL = '#', POINT = '.'};
+TILES map[MAP_VERTICAL][MAP_HORIZONTAL];
+bool run = true;
+
+enum INPUT {QUIT, UP, DOWN, LEFT, RIGHT, UNKNOWN };
+INPUT currentInput = INPUT::UNKNOWN;
+
+void Setup() {
+	for (size_t i = 0; i < MAP_VERTICAL; i++)
+	{
+		for (size_t j = 0; j < MAP_HORIZONTAL; j++)
+		{
+			if (i == 0 || j == 0 || i == MAP_VERTICAL - 1 || j == MAP_HORIZONTAL - 1) {
+				map[i][j] = TILES::WALL;
+			}
+			else {
+				map[i][j] = TILES::EMPTY;
+			}
+		}
+	}
+}
+
+
+void Input() {
+	char input;
+	std::cin >> input;
+	switch (input)
+	{
+	case 'w':
+	case 'W':
+		currentInput = INPUT::UP;
+		break;
+	case 's':
+	case 'S':
+		currentInput = INPUT::DOWN;
+		break;
+	case 'a':
+	case 'A':
+		currentInput = INPUT::LEFT;
+		break;
+	case 'd':
+	case 'D':
+		currentInput = INPUT::RIGHT;
+		break;
+	case 'q':
+	case 'Q':
+		currentInput = INPUT::QUIT;
+		break;
+	default:
+		currentInput = INPUT::UNKNOWN;
+		break;
+	}
+}
+void Logic() {
+
+	switch (currentInput)
+	{
+	case QUIT:
+		run = false;
+		break;
+	case UP:
+		break;
+	case DOWN:
+		break;
+	case LEFT:
+		break;
+	case RIGHT:
+		break;
+	}
+}
+void Draw() {
+	system("CLS");
+	for (size_t i = 0; i < MAP_VERTICAL; i++)
+	{
+		for (size_t j = 0; j < MAP_HORIZONTAL; j++)
+		{
+			std::cout << (char)map[i][j];
+		}
+		std::cout << std::endl;
+	}
+}
+
+int main()
+{
+	Setup();
+	Draw();
+	while (run) {
+		Input();
+		Logic();
+		Draw();
+	}
+}
